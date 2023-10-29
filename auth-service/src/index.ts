9aaa04475bdf8db99b0fb55d10c475105b1dc791
unknown <manshuchatrath20@gmail.com>
2023-10-28 21:23:54 -0400

    added sta

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(signUpRouter);
 app.use(loginRouter);
-
+app.get("/sda", (req: Request, res: Response) => {
+  return res.send({ message: "Logged Out!" });
+});
 sequelize
   .sync()
   .then((res) => {
